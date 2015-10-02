3ac3e30e5b0c915609023184c136f29537ca68ad
digitalxtasy <larry.l.sharpe@gmail.com>
2015-10-02 09:03:50 -0400

    Make sure broadcasters cant watch their own page

--- client/app/watch/watch.controller.js
+++ client/app/watch/watch.controller.js
@@ -1,8 +1,14 @@
 'use strict';
 
 angular.module('baseApp')
-  .controller('WatchCtrl', function ($scope, $http, $stateParams) {
+  .controller('WatchCtrl', function ($scope, $http, $stateParams, $location, Auth) {
     $scope.slug = $stateParams.slug;
+    var user = Auth.getCurrentUser();
+
+    if(Auth.hasRole('broadcaster') && $scope.slug === user.slug){
+      $location.path('/broadcast');
+    }
+
     $http.get('/api/users/broadcasters/'+$stateParams.slug)
       .success(function(data){
         $scope.broadcaster = data;
