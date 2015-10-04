8006348c58d521fc9ce019902fbc5d1c182b565b
digitalxtasy <larry.l.sharpe@gmail.com>
2015-10-03 21:52:59 -0400

    added rename album form

--- client/app/account/settings/albums.controller.js
+++ client/app/account/settings/albums.controller.js
@@ -1,6 +1,7 @@
 angular.module('baseApp')
   .controller('AlbumsCtrl', function ($scope, User, Auth, $http) {
     $scope.errors = {};
+    $scope.newAlbumName = '';
     $scope.createAlbumForm = false;
     $scope.openCreateAlbumForm = function (){
       $scope.createAlbumForm = true;
@@ -30,12 +31,25 @@ angular.module('baseApp')
       $scope.user.albums.splice(index,1);
     };
 
+    $scope.renameAlbum = function (index) {
+      $scope.user.albums[index].name = this.newAlbumName;
+      $scope.hideRenameForm(index);
+    };
+
     $scope.showUploadForm = function (index){
       $scope.user.albums[index].showUploadForm = true;
     };
 
     $scope.hideUploadForm = function (index){
       $scope.user.albums[index].showUploadForm = false;
+    };
+
+    $scope.showRenameForm = function (index){
+      $scope.user.albums[index].showRenameForm = true;
+    };
+
+    $scope.hideRenameForm = function (index){
+      $scope.user.albums[index].showRenameForm = false;
     };
 
   });
