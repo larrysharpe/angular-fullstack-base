21dc6183ca57d737eb2a38f29e3783fc88757eed
lsharpe15 <larry.sharpe@teamaol.com>
2016-04-08 01:39:47 -0400

    got room entry and departure working

---
+++ client/services/socketInit/socketInit.service.js
@@ -0,0 +1,42 @@
+'use strict';
+
+angular.module('baseApp')
+  .service('socketInit', function (socket, $state, $cookieStore) {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+
+
+    var initObj = {
+      page: '',
+      room: '',
+      remove: '',
+      user: {
+        slug: '',
+        username: '',
+        loggedIn: false
+      }
+    };
+
+    this.run = function (cb, user){
+
+      initObj.page = $state.current.name;
+
+      if (user.slug) initObj.user.slug = user.slug;
+      if (user.username) initObj.user.username = user.username;
+      if (user.roles) initObj.user.loggedIn =  true;
+      if ($state.params.slug && $state.current.room){
+        initObj.room = $state.params.slug + '-' + $state.current.room;
+      } else {
+        initObj.room = '';
+      }
+
+      /* Removes guest users from server after a user logs in */
+      if ($cookieStore.get('removeUser')) {
+        initObj.remove = $cookieStore.get('removeUser');
+        $cookieStore.remove('removeUser');
+      }
+
+      socket.emit('init', initObj, cb);
+
+    }
+
+  });
---
+++ client/services/socketInit/socketInit.service.spec.js
@@ -0,0 +1,17 @@
+'use strict';
+
+describe('Service: socketInit', function () {
+
+  // load the service's module
+  beforeEach(module('baseApp'));
+
+  // instantiate service
+  var socketInit;
+  beforeEach(inject(function (_socketInit_) {
+    socketInit = _socketInit_;
+  }));
+
+  it('should do something', function () {
+  });
+
+});
