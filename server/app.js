829b15f67c6cd4dda2ac65c8180c31cca31f735e
lsharpe15 <larry.sharpe@teamaol.com>
2017-03-11 23:34:10 -0500

    email setup

--- server/app.js
+++ server/app.js
@@ -33,5 +33,42 @@ server.listen(config.port, config.ip, function () {
   console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
 });
 
+
+
+var nodemailer = require('nodemailer');
+var xoauth2 = require('xoauth2');
+
+var xoauthoptions = {
+  user: 'admin@capital-mark.com',
+  clientId: '218668675591-58rbbt3v4k9pc1erilqtopmfiplg1t9p.apps.googleusercontent.com',
+  clientSecret: 'dXd3luCX-EIasUiEh-f0cMJE',
+  refreshToken: '1/SGR4zAhYdEFswHcckgsGn26jwspD8ki3JdNsjl_8Z3c'
+};
+
+var mailoptions = {
+  to: 'larry.l.sharpe@gmail.com',
+  from: 'admin@capital-mark.com',
+  subject: 'nodemailer test',
+  text: 'Hello World'
+};
+
+var transportoptions = {
+  service: 'gmail',
+  auth: {
+    xoauth2: xoauth2.createXOAuth2Generator(xoauthoptions)
+  }
+};
+
+var transporter = nodemailer.createTransport(transportoptions);
+transporter.sendMail(mailoptions, function(err, res){
+  if(err){
+    console.log('Error: ', err);
+  } else {
+    console.log('Email Sent');
+  }
+})
+
+
+
 // Expose app
 exports = module.exports = app;
