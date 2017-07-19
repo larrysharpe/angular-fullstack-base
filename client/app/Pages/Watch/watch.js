97004f96ee77be5cb8de5f2b8938712efb288e53
Larry Sharpe <larry.sharpe@teamaol.com>
2017-07-19 17:46:44 -0400

    Send username into broadcast.swf, changed watch to right .swf, added username to watch route

--- client/app/Pages/Watch/watch.js
+++ client/app/Pages/Watch/watch.js
@@ -4,9 +4,8 @@ angular.module('baseApp')
   .config(function ($stateProvider) {
     $stateProvider
       .state('Watch', {
-        url: '/watch',
+        url: '/watch/:username',
         templateUrl: 'app/Pages/Watch/watch.html',
-        controller: 'WatchCtrl',
-        authenticate: true
+        controller: 'WatchCtrl'
       });
   });
