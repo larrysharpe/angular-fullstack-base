38309daaf69d4f8c5d48a5da407caabef5d9f573
Larry Sharpe <larry.sharpe@teamaol.com>
2017-07-19 08:50:29 -0400

    got flash embed and communication working

--- client/app/Pages/Broadcast/broadcast.controller.js
+++ client/app/Pages/Broadcast/broadcast.controller.js
@@ -3,11 +3,25 @@
 angular.module('baseApp')
   .controller('BroadcastCtrl', function ($scope, User, Auth, $http) {
     $scope.errors = {};
-
+    $scope.vid = null;
     $scope.user = Auth.getCurrentUser();
 
+    $scope.status = 'Offline';
 
+    $scope.setStatus = function (status){
+      console.log('setStatusCalled', status);
+      $scope.status = status;
+    }
 
+    $scope.onLoadHandler = function (evt){
+      $scope.vid = evt.ref;
+    }
 
+    $scope.goPublic = function () {$scope.vid.callGoPublic();}
+    $scope.goAway = function () {$scope.vid.callGoAway();}
+    $scope.goOffline = function () {$scope.vid.callGoOffline();}
+    $scope.goPrivate = function () {$scope.vid.callGoPrivate();}
+    $scope.goVIP = function () {$scope.vid.callGoVIP();}
+    $scope.goPassword = function () {$scope.vid.callGoPassword();}
 
   });
